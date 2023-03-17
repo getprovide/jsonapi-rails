e96815605566a0f2a4742e408d03978ee061208b
Tim Wood <washwithcare@gmail.com>
2023-03-17 10:56:59 -0500

    Rename import back to lib/jsonapi/rails.rb to match gem name

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,7 +3,7 @@ require_relative 'boot'
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "jsonapi/rails_integration"
+require "jsonapi/rails"
 
 module Dummy
   class Application < Rails::Application
